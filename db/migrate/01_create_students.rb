863865f226adc5a0987cbc7200392d607769654e
Rafael Batista <rafaelb24@yahoo.com>
2019-01-03 22:01:47 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -9,6 +9,6 @@ class CreateStudents < ActiveRecord::Migration[5.1]
   ActiveRecord::Base.connection.execute(sql)
   
   def change
-    create_table students    
+    create_table students do |t|    
   end 
 end
